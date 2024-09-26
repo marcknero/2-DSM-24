c4e97b38450fc84956bc0415c2491eb4143ed4ba
Marcos V. <122242389+marcknero@users.noreply.github.com>
2024-09-25 21:47:59 -0300

    add atividadeAula6
    Exercícios resolvidos da aula 6 para avaliação do professor.

---
+++ AtividadeAula6/src/exercicio01.js
@@ -0,0 +1,11 @@
+function maior(x,y){
+    if(x>y){
+        return x;
+    } else {
+        return y;
+    };
+};
+m = maior(2,3);
+console.log(m);
+m = maior(5,2);
+console.log(m);
---
+++ AtividadeAula6/src/exercicio03.js
@@ -0,0 +1,7 @@
+function ultima (nome){
+    return nome[nome.length-1];
+};
+m = ultima("Pedros");
+console.log(m);
+m = ultima("Mariana");
+console.log(m);

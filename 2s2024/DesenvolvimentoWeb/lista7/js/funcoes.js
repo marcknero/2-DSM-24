d203d9ad53cb5222e85fd4a724c32751c4d5fe3f
Marcos <marck.nero@gmail.com>
2024-11-26 22:14:54 -0300

    update lista7

--- 2s2024/DesenvolvimentoWeb/lista7/js/funcoes.js
+++ 2s2024/DesenvolvimentoWeb/lista7/js/funcoes.js
@@ -1,6 +1,6 @@
 function exer1() {
     console.log("Carregou");
-  }
+  };
   
 // exercicio 02
 function exer2() {
@@ -8,13 +8,13 @@ function exer2() {
   entrada = document.querySelector("#entrada").value;
   // seta o corpo da div saida
   document.querySelector("#saida").innerText = entrada;
-}
+};
 
 // exercicio 03
 function exer3(){
   entrada = document.querySelector("#entrada").value;
   document.querySelector("#saida").value = entrada;
-}
+};
 
 //exercicio 04
 function exer4(){
@@ -22,8 +22,7 @@ function exer4(){
   const paragrafo = document.createTextNode("Marcos Vincius");
   document.querySelector('#saida').appendChild(p)
   p.appendChild(paragrafo)
-}
-
+};
 
 // exercicio 05
     //função alternativa, feita sem o createTextNode()
@@ -33,9 +32,10 @@ function exer4(){
       
 
       document.querySelector('#saida').appendChild(paragrafo)
-    }
+    };
 
-function exer5(){
+// exercicio 05
+    function exer5(){
   entrada = document.querySelector('#entrada').value;
   const p = document.createElement('p');
   const paragrafo = document.createTextNode(entrada);
@@ -43,4 +43,17 @@ function exer5(){
   p.appendChild(paragrafo)
   document.querySelector('#entrada').value = '';
   document.getElementById('entrada').focus()
-}
+};
+
+//exercicio 06
+function exer6(){
+  var entrada = document.querySelector('#entrada').value;
+  const p = document.createElement('p');
+  const paragrafo = document.createTextNode(entrada);
+  document.querySelector('#saida').appendChild(p);
+  
+  p.appendChild(paragrafo);
+
+  document.querySelector('#entrada').value = '';
+  document.getElementById('entrada').focus();
+};

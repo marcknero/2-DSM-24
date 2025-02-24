1f88091df916b557c1b9ce3e2063d318ae83311f
Marcos <marck.nero@gmail.com>
2025-02-24 19:46:13 -0300

    excluindo dados de atividade 1sem25

--- 1s2025/TecProgramacao/ExDir01/src/ex05.ts
+++
@@ -1,4 +0,0 @@
-const base: number = 10;
-const altura: number = 20;
-
-console.log(`A área do retangulo é: ${base * altura}`);
--- 1s2025/TecProgramacao/ExDir01/src/ex06.ts
+++
@@ -1,7 +0,0 @@
-const dia: number = 10;
-const mes: number = 5;
-const ano: number = 22;
-
-var total: number = ano*365 + mes*30 + dia;
-
-console.log(`Você tem ${total} dias de vida.`);

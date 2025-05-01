4e883775ff13ed15f4ecc4ad7f077b2226520f32
Marcos <marck.nero@gmail.com>
2025-05-01 02:08:45 -0300

    update atv4

--- 1s2025/web2/atv4/front/src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export function useTheme() {
-  const [darkTheme, setDarkTheme] = useState(true);
-
-  const toggleTheme = () => setDarkTheme((prev) => !prev);
-
-  return { darkTheme, toggleTheme };
-}
